2cc2926b137a8516d03dd1f9fa60157c17dbf00b
Khiem Nguyen <khiemcaleb@gmail.com>
2018-04-30 14:08:50 +0700

    Prevent empty name and lyrics values

--- Chorder.Angular/src/app/part/part.component.ts
+++ Chorder.Angular/src/app/part/part.component.ts
@@ -16,6 +16,24 @@ export class PartComponent {
   isFocus: boolean = true;
   DisplayMode: typeof DisplayMode = DisplayMode;
 
+  get isEmptyName(): boolean {
+    return !this.part.name && this.part.name.trim() == '';
+  }
+
+  onPartNameClick() {
+    if (!this.isReadOnly && !this.isEditing)
+      this.isEditing = true;
+  }
+
+  onPartNameBlur() {
+    if (this.isEditing && !this.isEmptyName) {
+      this.isEditing = false;
+
+      if (!this.isFocus)
+        this.isFocus = true;
+    }
+  }
+
 }
 
 export class Part {
