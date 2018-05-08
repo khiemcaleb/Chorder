0285e24658edd2f071ab44fae0e334a8d4692dc1
Khiem Nguyen <khiemcaleb@gmail.com>
2018-05-08 23:04:46 +0700

    Add ReducePartNameLength migration

--- Chorder.Api/Persistence/Migrations/20180508160412_ReducePartNameLength.Designer.cs
+++ Chorder.Api/Persistence/Migrations/20180508160412_ReducePartNameLength.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Chorder.Api.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180508160412_ReducePartNameLength")]
+    partial class ReducePartNameLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Chorder.Api/Persistence/Migrations/20180508160412_ReducePartNameLength.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Chorder.Api.Persistence.Migrations
+{
+    public partial class ReducePartNameLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Part",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Part",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}
